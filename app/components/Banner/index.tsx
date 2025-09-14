be815f297993f472596346c747ada952a7848669
Всеволод <grinvsevolod1@netnext.site>
2025-09-14 13:52:28 +0300

    изменил слово здарова

--- app/components/Banner/index.tsx
+++ app/components/Banner/index.tsx
@@ -13,7 +13,7 @@ const Banner = () => {
                     </div>
                     <div className="py-3 text-center lg:text-start">
                         <h1 className='text-6xl lg:text-80xl font-bold text-darkpurple'>
-                            Dedicated to <br /> bring your <br /> ideas to life.
+                            Здарова <br /> bring your <br /> ideas to life.
                         </h1>
                     </div>
                     <div className='my-7 text-center lg:text-start'>
